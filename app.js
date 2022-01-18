33672c7210a7f9b59eee9a295169b0553c422e63
Junya Qiao <junyaqiao@gmail.com>
2022-01-18 00:19:55 -0500

    add promise to page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //npm *(node package manager) -- npm init 
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -118,26 +118,40 @@ Add a New Project
       }
     ])
     .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
-      } else {
-        return portfolioData;
-      }
-    });
-};
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+  };
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+/*
     fs.writeFile('./index.html', pageHTML, err => {
       if (err) throw new Error(err);
 
       console.log('Page created! Check out index.html in this directory to see it!');
     });
-  });
+  });*/
 /**The first argument is the file name that will be created, or the output file. 
  * The second argument is the data that's being written: the HTML string template. 
  * The third argument is the callback function that will handle any errors as well as the success message. */
